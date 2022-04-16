6534377d7531f4741f2d5a25d4168c978e4d2346
Zhencha0Cai <caizhenchao@Cais-MacBook-Pro-2.local>
2022-04-16 00:10:22 -0500

    Remove unused component

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,9 +3,11 @@ import Dashboard from "../components/dashboard";
 import { Flex } from "@chakra-ui/react";
 const Home: NextPage = () => {
   return (
-    <Flex flexDir={"column"} width={["95%", "95%", "80%", "80%"]} mx={"auto"}>
-      <Dashboard />
-    </Flex>
+    <Flex
+      flexDir={"column"}
+      width={["95%", "95%", "80%", "80%"]}
+      mx={"auto"}
+    ></Flex>
   );
 };
 
